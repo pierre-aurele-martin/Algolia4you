719aebe72468378747e897537d1b43fc661853fc
Pierre Aurèle Martin <pierre.aurele.martin@gmail.com>
2017-09-22 17:02:02 +0200

    gruntfile comment cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,5 @@
 module.exports = function(grunt) {
 
-	//grunt.file.expandMapping
-	//<!-- include: "type": "css", "files": "<%= sources.all.css %>" -->
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sources:{
