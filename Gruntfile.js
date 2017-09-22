ce6854231c3d5b9471e6cdace9527b8fed8916c4
Pierre Aurèle Martin <pierre.aurele.martin@gmail.com>
2017-09-22 16:27:30 +0200

    index page is now templated

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,22 +6,45 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sources:{
+			pkg: '<%= pkg %>',
 			all: {
+				favicons: [
+					{rel: 'apple-touch-icon', sizes: '57x57', href: 'dist/img/favicon/apple-icon-57x57.png'},
+			        {rel: 'apple-touch-icon', sizes: '60x60', href: 'dist/img/favicon/apple-icon-60x60.png'},
+			        {rel: 'apple-touch-icon', sizes: '72x72', href: 'dist/img/favicon/apple-icon-72x72.png'},
+			        {rel: 'apple-touch-icon', sizes: '76x76', href: 'dist/img/favicon/apple-icon-76x76.png'},
+			        {rel: 'apple-touch-icon', sizes: '114x114', href: 'dist/img/favicon/apple-icon-114x114.png'},
+			        {rel: 'apple-touch-icon', sizes: '120x120', href: 'dist/img/favicon/apple-icon-120x120.png'},
+			        {rel: 'apple-touch-icon', sizes: '144x144', href: 'dist/img/favicon/apple-icon-144x144.png'},
+			        {rel: 'apple-touch-icon', sizes: '152x152', href: 'dist/img/favicon/apple-icon-152x152.png'},
+			        {rel: 'apple-touch-icon', sizes: '180x180', href: 'dist/img/favicon/apple-icon-180x180.png'},
+			        {rel: 'icon', sizes: '192x192', href: 'dist/img/favicon/android-icon-192x192.png'},
+			        {rel: 'icon', sizes: '32x32', href: 'dist/img/favicon/favicon-32x32.png'},
+			        {rel: 'icon', sizes: '96x96', href: 'dist/img/favicon/favicon-96x96.png'},
+			        {rel: 'icon', sizes: '16x16', href: 'dist/img/favicon/favicon-16x16.png'}
+			    ],
 				css: [
-					'maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
-					'css/open-iconic-bootstrap.min.css',
-					'fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic',
-					'css/app.min.css'
+					'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+					'//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic',
+					'dist/css/app.min.css'
 				],
-				js : [],
-			},
-			index: {
-				'css': ['css/app.min.css', 'css/autocomplete.css']
+				js : { 
+					'IE9': [
+						'https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js',
+        				'https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js'
+					],
+					'footer': [
+						'//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
+						'//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+						'dist/js/app.min.js'
+					]
+				},
+				navbar : grunt.file.read('src/tpl/navbar.tpl.html')
 			}
 		},
 		concat: {
 			options: {
-				separator: ';'
+				separator: ';',
 			},
 			dist: {
 				src: ['src/js/**/*.js'],
@@ -39,7 +62,7 @@ module.exports = function(grunt) {
 			}
 		},
 		jshint: {
-      		files: ['Gruntfile.js', 'src/**/*.js'],
+      		files: ['Gruntfile.js', 'src/js/*.js'],
 	      	options: {
 		        // options here to override JSHint defaults
 		        globals: {
@@ -50,10 +73,22 @@ module.exports = function(grunt) {
 		        }
 	      	}
 	    },
+	    template: {
+	        options: {
+	            // Task-specific options go here
+	        },
+	        dist: {
+	            options: {
+	            	data: '<%= sources %>'
+	            },
+	            files: {
+	                'index.html': ['src/tpl/index.tpl.html']
+	            }
+	        }
+	    },
 	    htmllint: {
-	    	src : ['index.html', 'algolia.html'],
+	    	src : ['*.html'],
 	    	options: {
-	    		force: true,
 	    		'attr-name-style': 'dash',
 	    		'id-class-style': 'dash'
 	    	}
@@ -89,6 +124,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-template');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', ['jshint','concat', 'uglify', 'sass', 'cssmin', 'htmllint']);
+	grunt.registerTask('default', ['concat', 'jshint', 'uglify', 'sass', 'cssmin', 'template', 'htmllint']);
 
 };
